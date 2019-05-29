093aa593b2c151a420a2f84d528a9c564bc9b786
Bartek Michalak <barmic12@gmail.com>
2019-05-29 23:45:10 +0200

    Disable auhentication for tags page.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,5 @@
 class FeedsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
   def index
     subscribed_users_ids = current_user.following.ids
     @posts = Post.subscribed(subscribed_users_ids)

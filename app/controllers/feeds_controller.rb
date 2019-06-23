e4d8e247b6b7a6dafc7eb1661d586c8f09b25963
Bartek Michalak <barmic12@gmail.com>
2019-06-23 11:20:35 +0200

    HTTP caching without DB queries caching.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -11,8 +11,10 @@ class FeedsController < ApplicationController
     # Following queries without where are really time-consuming
     # @comments_by_post = Comment.group(:post_id).count
     # @likes_by_post = @posts.joins(:likes).group('likes.likeable_id').count
-    
+
     @comments_by_post = Comment.where(post_id: @posts.ids).group(:post_id).count
     @likes_by_post = @posts.where(id: @posts.ids).joins(:likes).group('likes.likeable_id').count
+
+    fresh_when @posts
   end
 end
